c0daba8484da77941d62cdedacad5f4c22a7aac7
Anushka Dube <103627660+anushkadube@users.noreply.github.com>
2022-05-08 21:21:18 +0530

    Add files via upload

---
+++ sqrt_func.py
@@ -0,0 +1,29 @@
+
+
+#type 1
+import math
+x = 9
+y = math.sqrt(x)
+print("The input number is:",x, "\nThe square root of the number is:",y)
+
+
+#type 2
+import math as m
+x = 16
+y = m.sqrt(x)
+print("The input number is:",x, "\nThe square root of the number is:",y)
+
+
+#type 3
+from math import sqrt
+x = 5
+y = sqrt(x)
+print("The input number is:",x, "\nThe square root of the number is:",y)
+
+
+#type 4
+from math import *
+x = 5.10
+y = sqrt(x)
+z = floor(x)
+print("The input number is:",x, "\nThe square root of the number is:",y,"\nFloor:",z)
