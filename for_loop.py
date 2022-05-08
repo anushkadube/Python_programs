6cf51a65a663ba36a6b6c6f9832919776a42246c
Anushka Dube <103627660+anushkadube@users.noreply.github.com>
2022-05-08 21:20:05 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,34 @@
+
+range function
+iterator
+
+single - stop condition
+
+a = 10 ;
+
+for i in range(a):
+    print(i)
+
+#print(1)
+#print(2)
+#print(3)
+#print(4)
+#print(5)
+#print(6)
+#print(7)
+#print(8)
+#print(9)
+#print(10)
+
+n = 5
+for i in range(n):
+    print(i)
+
+n = 10
+for i in range(1,n+1): #for i in range(n): <- we did not used this condition
+    print(i)
+
+for i in range(2, 21, 2):  #table of 2
+    print(i)
+
+for i in range(l , m , n)
