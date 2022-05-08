5cdbb92949fe735237e67b5673b9ec67ad1fb2c9
Anushka Dube <103627660+anushkadube@users.noreply.github.com>
2022-05-08 21:22:17 +0530

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,13 @@
+
+p = [1,2,3,4,5,6,7,8,9,10]
+x = len(p)
+for i in range(x):
+    print(p[i])
+
+p = [1,2,3,4,5,6,7,8,9,10]
+# to find index of a value
+index = p.index(3)
+print("Index:",index)
+for i in p:
+    print (i)
+
